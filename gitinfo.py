d861f0649586d453ec13fc99e42ffbf60fcefa5a
Dorrabb Khan <dorrabbk@gmail.com>
2019-08-15 04:07:51 +0500

    Updated formatting, comments in code, further refactoring

--- gitinfo.py
+++ gitinfo.py
@@ -13,82 +13,127 @@ url_expression = r'(https?://)?(www.)?github.com/[a-zA-Z0-9-]+/[a-zA-Z0-9-]+'
 repo_expression = r'/[a-zA-Z0-9-]+/[a-zA-Z0-9-]+'
 # expression to create url for Github API
 
+
 class Repository:
+    # create class for information of repository
 
     def __init__(self, url):
+        # initialize URL
+
         self.url = url
-        self.get_repo_info()
+        # set the repository's URL
+
+        self.set_repo_info()
+        # obtain repository's information
 
     @property
     def url(self):
+        # getter function for the URL
         return self._url
 
     @url.setter
     def url(self, url):
+        # set URL after running regex test on it
+
         if not search(url_expression, url):
             raise ValueError("Invalid Github repo URL")
-        self._url = url
+            # raise error if the input URL does not match the regex pattern
 
-    def get_repo_info(self):
+        self._url = url
+        # set url
+
+    def set_repo_info(self):
+        # function to obtain the repository's information from the web
+        # and store it in this object
+
         match = search(repo_expression, self.url)
         # extract the /user/repo part from the url received in input
 
         output_url = 'https://api.github.com/repos' + match.group()
-        # append /user/repo to Github API's url
+        # append /user/repo to Github API's URL
 
         response = get(output_url, headers={"Accept": "application/json"})
         # request the repository's JSON from the Github API
 
         if response.status_code != 200:
             raise RuntimeError("Error reaching the repository")
-            # exit if the request was not successful
+            # raise error if the request was not successful
 
         self._repo_data = response.json()
-        # parse the obtained JSON
+        # parse the obtained JSON and store it
 
     def name(self):
+        # return the repository's name
+
         return self._repo_data["name"]
 
     def owner(self):
+        # return the repository's owner's username
+
         return self._repo_data["owner"]["login"]
 
     def description(self):
+        # return the repository's description
+
         return self._repo_data["description"]
 
     def is_forked(self):
+        # return whether the repository is forked
+
         return self._repo_data["fork"] != "true"
 
     def created_at(self):
+        # return the repository's creation date and time
+
         return self._repo_data["created_at"]
 
     def size(self):
+        # return the repository's size in KB
+
         return int(self._repo_data["size"])
 
     def language(self):
+        # return the repository's language
+
         return self._repo_data["language"]
 
     def watchers(self):
+        # return the repository's number of watchers
+
         return int(self._repo_data["watchers"])
 
     def stars(self):
+        # return the repository's number of stars
+
         return int(self._repo_data["stargazers_count"])
 
     def forks(self):
+        # return the repository's number of forks
+
         return int(self._repo_data["forks_count"])
 
     def open_issues(self):
+        # return the repository's number of open issues
+
         return int(self._repo_data["open_issues"])
 
     def default_branch(self):
+        # return the repository's default branch name
+
         return self._repo_data["default_branch"]
 
     def subscribers(self):
+        # return the repository's number of subscribers
+
         return self._repo_data["subscribers_count"]
 
+
 if __name__ == "__main__":
+    # execute only if code is not imported
 
     url = input("Enter a URL: ")
     new = Repository(url)
+    # input URL and initialize the repository object
 
     print(f'\nName: {new.name()}')
     print(f'Owner: {new.owner()}')
