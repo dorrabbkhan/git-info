738e1719d961d8b42cc194fd6d828b9fe9bde9b7
Dorrabb Khan <dorrabbk@gmail.com>
2019-08-15 20:54:44 +0500

    Added unittest for checking regex for correct URL inputs

--- test.py
+++ test.py
@@ -1,19 +1,44 @@
-from gitinfo import repository
+import gitinfo
+import unittest
+import re
 
-new = repository("https://github.com/dorrabbkhan/GitSizeViewer")
+base_urls = [
 
-print(f'\nName: {new.name()}')
-print(f'Owner: {new.owner()}')
-print(f'Description: {new.description()}')
-print(f'Is Forked: {new.is_forked()}')
-print(f'Created At: {new.created_at()}')
-print(f'Size: {round(new.size()/1024, 3)} MB')
-print(f'Language: {new.language()}')
-print(f'Watchers: {new.watchers()}')
-print(f'Stars: {new.stars()}')
-print(f'Forks: {new.forks()}')
-print(f'Open Issues: {new.open_issues()}')
-print(f'Default Branch: {new.default_branch()}')
-print(f'Subscribers: {new.subscribers()}')
+    "https://github.com/dorrabbkhan/git-info",
+    "http://github.com/dorrabbkhan/git-info",
+    "https://www.github.com/dorrabbkhan/git-info",
+    "http://www.github.com/dorrabbkhan/git-info",
+    "www.github.com/dorrabbkhan/git-info",
+    "github.com/dorrabbkhan/git-info"
+    
+]
 
-input()
+class TestRepo(unittest.TestCase):
+
+    def test_url_regex(self):
+
+        for url in base_urls:
+            self.assertIsNotNone(re.search(gitinfo.url_expression, url))
+
+    def test_output_url_regex(self):
+
+        for url in base_urls:
+            self.assertIsNotNone(re.search(gitinfo.repo_expression, url))
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+# print(f'\nName: {new.name()}')
+# print(f'Owner: {new.owner()}')
+# print(f'Description: {new.description()}')
+# print(f'Is Forked: {new.is_forked()}')
+# print(f'Created At: {new.created_at()}')
+# print(f'Size: {round(new.size()/1024, 3)} MB')
+# print(f'Language: {new.language()}')
+# print(f'Watchers: {new.watchers()}')
+# print(f'Stars: {new.stars()}')
+# print(f'Forks: {new.forks()}')
+# print(f'Open Issues: {new.open_issues()}')
+# print(f'Default Branch: {new.default_branch()}')
+# print(f'Subscribers: {new.subscribers()}')
