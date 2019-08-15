27faec569b7fd9608a8fba3473875c5f022dc48e
Adoria298 <bananaorange29@outlook.com>
2019-08-15 19:19:29 +0100

    Add index.html

--- gitinfo-web.py
+++ gitinfo-web.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 from gitinfo import Repository
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello Github"
+    return render_template("index.html")
 
 app.run(host="127.0.0.1", port=5000, debug=True) 
