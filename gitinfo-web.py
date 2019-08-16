ca5aef206df987e4959054f850a49b54b0ee6e50
Dorrabb Khan <dorrabbk@gmail.com>
2019-08-17 03:05:05 +0500

    Removed bugs and bad code, pylint cleanup of Adoria298's gitinfo-web.py
    - Score upped from 2.79 to 9.00
    - PEP8 cleanup
    - Faster emptying of lists
    - Lists emptied in try block
    - Better formatting
    - Replaced eval() with getattr() implementation
    - took return statement out of finally block to avoid lost exceptions
    - Better variable names

--- gitinfo-web.py
+++ gitinfo-web.py
@@ -1,50 +1,71 @@
 """
-Runs a Flask webserver which allows you to view information about a Github 
-repository, which is obtained using the gitinfo.Repository class. Depends on 
+Runs a Flask webserver which allows you to view information about a Github
+repository, which is obtained using the gitinfo.Repository class. Depends on
 the flask package, and the gitinfo module. Will dynamically add new information
 based on the available functions of said class.
 """
+
 from flask import Flask, render_template, request
-from gitinfo import Repository
+from gitinfo import repository
 
-app = Flask(__name__)
+APP = Flask(__name__)
+DATA = []
+# initialize Flask app, list of dictionaries
 
-data = [] # list of dictionaries
-
-@app.route("/")
+@APP.route("/")
 def index():
     """
     Index page which renders any information given to it.
     """
-    return render_template("index.html", repo_data=data)
 
-# logic for 
-@app.route("/get_data", methods=["POST"])
+    return render_template("index.html", repo_data=DATA)
+
+@APP.route("/get_data", methods=["POST"])
 def get_data():
     """
     Gets the repo data, formats it into data, renders the index page.
     """
-    while len(data) != 0: data.pop() # empties the list
-    url = request.form['url'] # gets the URL from the input box on index.html
-    if not url.startswith("https://www.github.com/"):
-        url = "https://www.github.com/" + url # allows 'user/repo' format
-    try:
-        repo = Repository(url)
-        for name in [d for d in dir(repo) if not d.startswith("_") 
-            and callable(getattr(repo, d))]: 
-            # only public functions used
-            datum = { # format required by index.html
-                "name": name,
-                "info": eval("repo."+name+"()")
-            }
-            data.append(datum)
-    except Exception as e: # in case of errors, tells the client
-        data.append({
-            "name": "Error",
-            "info": e
-        })
-    finally:
-        return index()
 
+    url = request.form['url']
+    # gets the URL from the input box on index.html
+
+    if not url.startswith("https://www.github.com/"):
+        url = "https://www.github.com/" + url
+        # allows 'user/repo' format
+
+    try:
+        DATA.clear()
+        # empties the list
+
+        repo = repository(url)
+        for name in [
+                function for function in dir(repo)
+                if not function.startswith("_")
+                and callable(getattr(repo, function))
+            ]:
+            # only public functions from repo object used
+            # which do not start with _ and are callable
+
+            datum = {
+                "name": name,
+                "info": getattr(repo, name)()
+            }
+            # format required by index.html
+
+            DATA.append(datum)
+            # append to data to be displayed on index.html
+
+    except Exception as error:
+        # in case of errors, tells the client
+
+        DATA.append({
+            "name": "Error",
+            "info": error
+        })
+        # append to data to be displayed on index.html
+
+    return index()
+    # return data to the webpage
+
+APP.run(host="127.0.0.1", port=5000, debug=True)
 # runs the webserver locally in debug mode (which presents errors prettily)
-app.run(host="127.0.0.1", port=5000, debug=True) 
